4c799e6ce08b00be4552f3ce7d8f8afe64a8a73b
lukasw@audentio.com <lukasw@audentio.com>
2019-05-08 14:05:08 +0200

    Moved app_setup to app_admin_setup

--- Listener/AppSetup.php
+++ Listener/AppSetup.php
@@ -8,19 +8,5 @@ class AppSetup
 {
     public static function appSetup(App $app)
     {
-        $session = $app->session();
-
-        if($session->offsetExists('th_iau_bounce')) {
-            $bounce = $session->offsetGet('th_iau_bounce');
-
-            if($bounce) {
-                $session->offsetUnset('th_iau_encryption_secret');
-                $session->offsetUnset('th_iau_tfa_code');
-                $session->offsetUnset('th_iau_bounce');
-            }
-            else {
-                $session->offsetSet('th_iau_bounce', 1);
-            }
-        }
     }
 }
